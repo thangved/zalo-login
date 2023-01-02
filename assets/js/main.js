5f80973308e6de231929caa38bc63fef781368fc
Kim Minh Thang <thangved02@gmail.com>
2023-01-02 17:47:03 +0700

    Format

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
-const tabs = document.querySelectorAll('.tabs');
+const tabs = document.querySelectorAll(".tabs");
 
 tabs.forEach((tabs) => {
-	const tabButtons = tabs.querySelectorAll('.tab');
+	const tabButtons = tabs.querySelectorAll(".tab");
 	tabButtons.forEach((tabButton) => {
-		tabButton.addEventListener('click', () => {
+		tabButton.addEventListener("click", () => {
 			tabButtons.forEach((tabButton) => {
-				tabButton.classList.remove('active');
+				tabButton.classList.remove("active");
 				const target = document.querySelector(
-					tabButton.getAttribute('data-target'),
+					tabButton.getAttribute("data-target")
 				);
-				if (target) target.style.display = 'none';
+				if (target) target.style.display = "none";
 			});
 
-			tabButton.classList.add('active');
+			tabButton.classList.add("active");
 			const target = document.querySelector(
-				tabButton.getAttribute('data-target'),
+				tabButton.getAttribute("data-target")
 			);
-			if (target) target.style.display = 'block';
+			if (target) target.style.display = "block";
 		});
 	});
 });
